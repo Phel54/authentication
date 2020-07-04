7f163a1a0259e16c64cf8fe1eb0da8b2a5ccad9d
Phelim <isiphelim@gmail.com>
2020-07-04 10:26:26 +0000

    added config db

---
+++ server.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const log = require('morgan')('dev');
+const express = require('express');
+const cors = require('cors');
+
+var app = express();
+
+// Mongo Database
+const db = require('./config/config.db');
+
+// Call the database connectivity function
+db();
+
+// Configure bodyparser
+const bodyParserJSON = bodyParser.json();
+const bodyParserURLEncoded = bodyParser.urlencoded({ extended: true });
+const bodyParserRaw = bodyParser.raw();
+const bodyParserText = bodyParser.text();
+
+// Configure app.use()
+app.use(log);
+app.use(express.json());
+app.use(bodyParserJSON);
+app.use(bodyParserURLEncoded);
+app.use(bodyParserRaw);
+app.use(bodyParserText);
+app.use(cors());
+
+// Initialise Express Router
+const router = express.Router();
+app.use('/api/v1', router);
+
+// Admin Module
+// -
+
+app.get('/api/v1', (req, res) =>
+	res.json({ message: 'Hello World! Welcome to iDeyPay Till Engine' })
+);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+	console.log(`server started on port ${PORT}`);
+});
